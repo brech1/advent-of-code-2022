a471428affa7d21212e4505f473297831c081852
Brechy <brechy.web3@gmail.com>
2022-12-02 09:34:03 -0300

    complete: day 2

--- exercises/day-2/src/main.rs
+++ exercises/day-2/src/main.rs
@@ -9,7 +9,9 @@ enum Play {
 }
 
 // Play outcome with points
+#[derive(PartialEq)]
 enum Outcome {
+    Loss = 0,
     Draw = 3,
     Win = 6,
 }
@@ -20,6 +22,7 @@ fn main() {
 
     // Initialize vec
     let mut p1_scores: Vec<u32> = Vec::new();
+    let mut p2_scores: Vec<u32> = Vec::new();
 
     // Get points
     let mut get_points = |line: &str| {
@@ -30,6 +33,11 @@ fn main() {
         p1_scores.push(score);
 
         // Round Two
+        let p2_round = get_p2_round(line);
+
+        let p2_score = get_p2_score(p2_round);
+
+        p2_scores.push(p2_score);
     };
 
     // Split lines
@@ -38,6 +46,7 @@ fn main() {
     lines.for_each(|x| get_points(x));
 
     println!("Part One Total: {:?}", p1_scores.into_iter().sum::<u32>());
+    println!("Part Two Total: {:?}", p2_scores.into_iter().sum::<u32>());
 }
 
 fn get_p1_score(round: (Play, Play)) -> u32 {
@@ -70,10 +79,38 @@ fn get_p1_score(round: (Play, Play)) -> u32 {
     return score;
 }
 
+fn get_p2_score(round: (Play, Outcome)) -> u32 {
+    let opponent = round.0;
+    let outcome = round.1;
+    let mut score = outcome as u32;
+
+    match outcome {
+        Outcome::Loss => match opponent {
+            Play::Rock => score += Play::Scissors as u32,
+            Play::Paper => score += Play::Rock as u32,
+            Play::Scissors => score += Play::Paper as u32,
+        },
+        Outcome::Draw => score += opponent as u32,
+        Outcome::Win => match opponent {
+            Play::Rock => score += Play::Paper as u32,
+            Play::Paper => score += Play::Scissors as u32,
+            Play::Scissors => score += Play::Rock as u32,
+        },
+    }
+
+    return score;
+}
+
 fn get_p1_round(line: &str) -> (Play, Play) {
     let plays = line.split(" ").collect::<Vec<&str>>();
 
     return (get_p1_play(plays[0]), get_p1_play(plays[1]));
+}
+
+fn get_p2_round(line: &str) -> (Play, Outcome) {
+    let plays = line.split(" ").collect::<Vec<&str>>();
+
+    return (get_p1_play(plays[0]), get_outcome(plays[1]));
 }
 
 fn get_p1_play(code: &str) -> Play {
@@ -84,6 +121,15 @@ fn get_p1_play(code: &str) -> Play {
         "X" => return Play::Rock,
         "Y" => return Play::Paper,
         "Z" => return Play::Scissors,
+        _ => todo!(),
+    }
+}
+
+fn get_outcome(code: &str) -> Outcome {
+    match code {
+        "X" => return Outcome::Loss,
+        "Y" => return Outcome::Draw,
+        "Z" => return Outcome::Win,
         _ => todo!(),
     }
 }
