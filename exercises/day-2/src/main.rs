2d6c0906b4db9231e1db0e9b06f4302c043b6fa2
Brechy <brechy.web3@gmail.com>
2022-12-02 09:04:46 -0300

    add: part two

--- exercises/day-2/src/main.rs
+++ exercises/day-2/src/main.rs
@@ -8,25 +8,28 @@ enum Play {
     Scissors = 3,
 }
 
-const WIN_POINTS: u32 = 6;
-const DRAW_POINTS: u32 = 3;
-
-// Wrong guess: 9302
+// Play outcome with points
+enum Outcome {
+    Draw = 3,
+    Win = 6,
+}
 
 fn main() {
     // Read file
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
 
     // Initialize vec
-    let mut scores: Vec<u32> = Vec::new();
+    let mut p1_scores: Vec<u32> = Vec::new();
 
     // Get points
     let mut get_points = |line: &str| {
-        let round = get_round(line);
+        let round = get_p1_round(line);
 
-        let score = get_score(round);
+        let score = get_p1_score(round);
 
-        scores.push(score);
+        p1_scores.push(score);
+
+        // Round Two
     };
 
     // Split lines
@@ -34,34 +37,32 @@ fn main() {
 
     lines.for_each(|x| get_points(x));
 
-    println!("All hands: {:?}", scores);
-
-    println!("Total: {:?}", scores.into_iter().sum::<u32>());
+    println!("Part One Total: {:?}", p1_scores.into_iter().sum::<u32>());
 }
 
-fn get_score(round: (Play, Play)) -> u32 {
+fn get_p1_score(round: (Play, Play)) -> u32 {
     let opponent = round.0;
     let own = round.1;
     let mut score = own as u32;
 
     if opponent == own {
-        return score + DRAW_POINTS;
+        return score + Outcome::Draw as u32;
     }
 
     match opponent {
         Play::Rock => {
             if own == Play::Paper {
-                score += WIN_POINTS;
+                score += Outcome::Win as u32;
             }
         }
         Play::Paper => {
             if own == Play::Scissors {
-                score += WIN_POINTS;
+                score += Outcome::Win as u32;
             }
         }
         Play::Scissors => {
             if own == Play::Rock {
-                score += WIN_POINTS;
+                score += Outcome::Win as u32;
             }
         }
     }
@@ -69,13 +70,13 @@ fn get_score(round: (Play, Play)) -> u32 {
     return score;
 }
 
-fn get_round(line: &str) -> (Play, Play) {
+fn get_p1_round(line: &str) -> (Play, Play) {
     let plays = line.split(" ").collect::<Vec<&str>>();
 
-    return (get_play(plays[0]), get_play(plays[1]));
+    return (get_p1_play(plays[0]), get_p1_play(plays[1]));
 }
 
-fn get_play(code: &str) -> Play {
+fn get_p1_play(code: &str) -> Play {
     match code {
         "A" => return Play::Rock,
         "B" => return Play::Paper,
