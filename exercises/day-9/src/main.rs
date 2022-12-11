64e549a201fe660f604d4d8e67dc3aad5f148b64
Brechy <brechy.web3@gmail.com>
2022-12-10 21:33:49 -0300

    complete: day 9

--- exercises/day-9/src/main.rs
+++ exercises/day-9/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, fs};
 
-const KNOTS: u32 = 10;
+const KNOTS: u32 = 9;
 
 #[derive(Debug, Clone)]
 enum Direction {
@@ -16,6 +16,7 @@ struct Position {
     y: i32,
 }
 
+#[derive(Debug)]
 struct Command {
     direction: Direction,
     steps: u32,
@@ -24,8 +25,13 @@ struct Command {
 #[derive(Debug, Clone)]
 struct Rope {
     head: Position,
-    tail: Position,
-    tail_history: HashMap<Position, bool>,
+    knots: Vec<Knot>,
+}
+
+#[derive(Debug, Clone)]
+struct Knot {
+    pos: Position,
+    history: HashMap<Position, bool>,
 }
 
 impl Position {
@@ -35,127 +41,99 @@ impl Position {
             self.y.abs_diff(new_position.y),
         ];
     }
+
+    fn follow(&self, head: &Position) -> Position {
+        let mut new_pos = self.clone();
+
+        let [x_distance, y_distance] = self.get_distance(&head);
+
+        if x_distance > 1 {
+            if self.x > head.x {
+                new_pos.x -= 1;
+            } else {
+                new_pos.x += 1;
+            }
+
+            if y_distance == 1 {
+                if self.y > head.y {
+                    new_pos.y -= 1;
+                } else {
+                    new_pos.y += 1;
+                }
+            }
+        }
+
+        if y_distance > 1 {
+            if self.y > head.y {
+                new_pos.y -= 1;
+            } else {
+                new_pos.y += 1;
+            }
+
+            if x_distance == 1 {
+                if self.x > head.x {
+                    new_pos.x -= 1;
+                } else {
+                    new_pos.x += 1;
+                }
+            }
+        }
+
+        return new_pos;
+    }
 }
 
 impl Rope {
     fn new() -> Rope {
-        let mut tail_history: HashMap<Position, bool> = HashMap::new();
+        let mut knots = Vec::new();
 
-        tail_history.insert(Position { x: 0, y: 0 }, true);
+        for _ in 0..KNOTS {
+            let mut history: HashMap<Position, bool> = HashMap::new();
+            history.insert(Position { x: 0, y: 0 }, true);
+
+            knots.push(Knot {
+                pos: Position { x: 0, y: 0 },
+                history,
+            })
+        }
 
         Rope {
             head: Position { x: 0, y: 0 },
-            tail: Position { x: 0, y: 0 },
-            tail_history,
+            knots,
         }
     }
 
     fn move_head(&mut self, command: Command) {
-        match command.direction {
-            Direction::Left => self.move_left(command.steps),
-            Direction::Right => self.move_right(command.steps),
-            Direction::Up => self.move_up(command.steps),
-            Direction::Down => self.move_down(command.steps),
-        }
-    }
-
-    fn move_left(&mut self, steps: u32) {
-        for _ in 0..steps {
-            self.head.x -= 1;
-
-            // Update tail
-            // Check distance
-            let [x_distance, y_distance] = self.head.get_distance(&self.tail);
-
-            if x_distance > 1 || y_distance > 1 {
-                // Check if same row
-                if self.head.y == self.tail.y {
-                    self.tail.x -= 1;
-                } else {
-                    self.tail.x -= 1;
-                    self.tail.y = self.head.y;
-                }
-
-                // Record Tail Position
-                self.tail_history.insert(self.tail.clone(), true);
+        for _ in 0..command.steps {
+            match command.direction {
+                Direction::Left => self.head.x -= 1,
+                Direction::Right => self.head.x += 1,
+                Direction::Up => self.head.y += 1,
+                Direction::Down => self.head.y -= 1,
             }
-        }
-    }
 
-    fn move_right(&mut self, steps: u32) {
-        for _ in 0..steps {
-            self.head.x += 1;
+            println!("\n\n-------------------------\n\n");
+            println!("Head -- {:?}", self.head);
 
-            // Update tail
-            // Check distance
-            let [x_distance, y_distance] = self.head.get_distance(&self.tail);
+            // Update knots
+            for i in 0..self.knots.len() {
+                let new_pos: Position;
 
-            if x_distance > 1 || y_distance > 1 {
-                // Check if same row
-                if self.head.y == self.tail.y {
-                    self.tail.x += 1;
+                if i == 0 {
+                    new_pos = self.knots[i].pos.follow(&self.head);
                 } else {
-                    self.tail.x += 1;
-                    self.tail.y = self.head.y;
+                    new_pos = self.knots[i].pos.follow(&self.knots[i - 1].pos)
                 }
 
-                // Record Tail Position
-                self.tail_history.insert(self.tail.clone(), true);
-            }
-        }
-    }
+                self.knots[i].pos = new_pos.clone();
 
-    fn move_up(&mut self, steps: u32) {
-        for _ in 0..steps {
-            self.head.y += 1;
+                self.knots[i].history.insert(new_pos, true);
 
-            // Update tail
-            // Check distance
-            let [x_distance, y_distance] = self.head.get_distance(&self.tail);
-
-            if x_distance > 1 || y_distance > 1 {
-                // Check if same column
-                if self.head.x == self.tail.x {
-                    self.tail.y += 1;
-                } else {
-                    self.tail.y += 1;
-                    self.tail.x = self.head.x;
-                }
-
-                // Record Tail Position
-                self.tail_history.insert(self.tail.clone(), true);
-            }
-        }
-    }
-
-    fn move_down(&mut self, steps: u32) {
-        for _ in 0..steps {
-            self.head.y -= 1;
-
-            // Update tail
-            // Check distance
-            let [x_distance, y_distance] = self.head.get_distance(&self.tail);
-
-            if x_distance > 1 || y_distance > 1 {
-                // Check if same column
-                if self.head.x == self.tail.x {
-                    self.tail.y -= 1;
-                } else {
-                    self.tail.y -= 1;
-                    self.tail.x = self.head.x;
-                }
-
-                // Record Tail Position
-                self.tail_history.insert(self.tail.clone(), true);
+                println!("Knot {}, {:?}", i + 1, self.knots[i].pos);
             }
         }
     }
 }
-
-// if the head and tail aren't touching and aren't in the same row or column
-// the tail always moves one step diagonally to keep up
-
-// 6351 - too high
 
 fn main() {
     // Read input
@@ -164,74 +142,17 @@ fn main() {
     // Get input lines
     let lines = input.lines().into_iter();
 
-    // Init Rope
-    let mut rope = Rope::new();
-
-    for line in lines.clone() {
-        rope.move_head(parse_command(line));
-    }
-
-    println!("Part one: {}", rope.tail_history.len());
-
-    // Part Two -- Simulate knots with ropes
-
     // Initialize Ropes
-    let mut ropes: Vec<Rope> = Vec::new();
-
-    for _ in 0..KNOTS {
-        ropes.push(Rope::new());
-    }
+    let mut rope = Rope::new();
 
     // Execute commands
 
-    for line in lines.clone() {
-        ropes[0].move_head(parse_command(line));
-
-        for i in 1..KNOTS as usize {
-            // Adjust head for the tail of the next rope
-            let next_tail = ropes[i - 1].tail.clone();
-
-            // Get difference from head with next tail
-            let x_difference = ropes[i].head.x.abs_diff(next_tail.x);
-            let y_difference = ropes[i].head.y.abs_diff(next_tail.y);
-
-            if x_difference > 0 {
-                if next_tail.x > ropes[i].head.x {
-                    ropes[i].move_head(Command {
-                        direction: Direction::Right,
-                        steps: x_difference,
-                    });
-                } else {
-                    ropes[i].move_head(Command {
-                        direction: Direction::Left,
-                        steps: x_difference,
-                    });
-                }
-            }
-
-            if y_difference > 0 {
-                if next_tail.y > ropes[i].head.y {
-                    ropes[i].move_head(Command {
-                        direction: Direction::Up,
-                        steps: y_difference,
-                    });
-                } else {
-                    ropes[i].move_head(Command {
-                        direction: Direction::Down,
-                        steps: y_difference,
-                    });
-                }
-            }
-
-            if ropes[i].head != next_tail {
-                println!("not working")
-            }
-        }
+    for line in lines {
+        rope.move_head(parse_command(line));
     }
 
-    println!("Part two: {}", ropes[9].tail_history.len());
-
-    // 2909 - Too high
+    println!("Part one: {}", rope.knots[0].history.len());
+    println!("Part two: {}", rope.knots[8].history.len());
 }
 
 fn parse_command(line: &str) -> Command {
